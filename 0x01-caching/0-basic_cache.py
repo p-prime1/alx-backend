dd68c19df61d65efa798e965e2cda25898bb1cb8
p-prime1 <preciousnwaka95@gmail.com>
2024-10-29 22:48:44 +0100

    implemented the get and put method in a child class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from base_caching import BaseCaching
+"""Module contains BasicCache class"""
+
+
+class BasicCache(BaseCaching):
+    """Child class that inherits from BasicCaching"""
+
+    def __init__(self):
+        """Initialize"""
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves the item associated with key"""
+        if key:
+            return self.cache_data.get(key)
+        return None
