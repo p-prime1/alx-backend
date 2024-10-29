598a2a9399e53f3d18554a919d0204f8cb97884f
p-prime1 <preciousnwaka95@gmail.com>
2024-10-29 23:46:18 +0100

    get method uses fifo algorithm when data is more than max items

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from base_caching import BaseCaching
+from collections import OrderedDict
+"""Module contains FIFOCache class"""
+
+
+class FIFOCache(BaseCaching):
+    """Child class that inherits from BasicCaching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                pop_item = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {pop_item[0]}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves the item associated with key"""
+        if key:
+            return self.cache_data.get(key)
+        return None
