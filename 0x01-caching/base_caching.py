d9b90091ae2fd62d0c9bf01652417ee553a5b9c9
p-prime1 <preciousnwaka95@gmail.com>
2024-10-29 17:41:58 +0100

    added parent class

---
+++ 0x01-caching/base_caching.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    BaseCaching module
+"""
+
+
+class BaseCaching():
+    """BaseCaching defines:
+    - constants of the caching system
+    - where data are stored (in a dictionary)
+    """
+
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """Inittialize
+        """
+        self.cache.data = {}
+
+    def print_cache(self):
+        """Print the cache
+        """
+        print("Current cache:")
+        from key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+            
+    def put(self, key, item):
+        """Addan item in the cache
+        """
+        raise NotImplementedError("put must be implemented in your cache class")
+    
+    def get(self, key):
+        """Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in your cache class")
