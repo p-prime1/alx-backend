c0a4879e36e2f909bf13c812073559f9b3611523
p-prime1 <preciousnwaka95@gmail.com>
2024-10-30 22:17:12 +0100

    class uses last recently used algorithm to remove cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+"""Module contains FIFOCache class"""
+
+
+class LRUCache(BaseCaching):
+    """Child class that inherits from BasicCaching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                pop_item = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {pop_item[0]}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves the item associated with key"""
+        self.use_count = {}
+        if key:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            return self.cache_data.get(key)
+        return None
