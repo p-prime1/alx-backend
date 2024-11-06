20ac551c1b6d32c75837c063f27787f4b2739b97
p-prime1 <preciousnwaka95@gmail.com>
2024-11-06 16:15:23 +0100

    added proper documentation and used pycodestyle compliant code

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
+"""Module contains the Config class which contains all the supported
+    languages, the home fucn renders the html file, module instantiates
+    babel also"""
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Class sets babels default locale to 'en' and timezone to 'UTC'"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 @app.route('/')
 def home():
+    """Renders the html  file"""
     return render_template('0-index.html')
 
 
