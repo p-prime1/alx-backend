2d925d448097b3eadebe1f8ad768e4c9f863827f
p-prime1 <preciousnwaka95@gmail.com>
2024-11-06 16:06:24 +0100

    added shebang and used pycodestyle compliant code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+"""Module inititalizes contains Config class and instantiates babel"""
+
 from flask import Flask, render_template
 from flask_babel import Babel
-"""Module inititalizes contains Config class and instantiates babel"""
 
 
 class Config:
@@ -9,12 +11,17 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 
 babel = Babel(app)
-@app.route('/')
 
+
+@app.route('/')
 def home():
+    """Renders the html file"""
     return render_template('0-index.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
